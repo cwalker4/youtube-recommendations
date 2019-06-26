ae746667a7e9c2313bc325f951b8821eb95f5b79
Charlie Walker <cwalker4@stanford.edu>
2019-06-26 17:51:42 -0400

    bugfixes

--- repeat_search.py
+++ repeat_search.py
@@ -7,11 +7,17 @@ import os
 
 from youtube_follower import youtube_follower
 
+results_dir = 'data/scrape_results_old'
+
 for tree_dir in os.listdir(results_dir):
     tree_path = os.path.join(results_dir, tree_dir)
+    # skip non-search directories/files
     if tree_dir in ['video_info.json', 'captions']:
         continue
-    query, root_id = scrape_dir.split('_', maxsplit=1)
+    # skip search if already repeated
+    if tree_dir in os.listdir('data/scrape_results_redo'):
+    	continue
+    query, root_id = tree_dir.split('_', maxsplit=1)
     
     print("Re-running tree with query {} and root {}".format(query, root_id))
     with open(os.path.join(tree_path, 'params.json')) as f:
