1eb29f9886b8f0ca7d64c6da535b614415fade8c
Charlie Walker <cwalker4@stanford.edu>
2019-07-23 11:20:27 -0700

    reduce play time

--- youtube_follower/youtube_follower_selenium.py
+++ youtube_follower/youtube_follower_selenium.py
@@ -180,9 +180,12 @@ class YoutubeFollower():
 
 
     def skip_ads(self):
-        wait = WebDriverWait(self.browser, 60)
-        # press play on the video
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='ytp-play-button ytp-button']"))).click()
+        wait = WebDriverWait(self.browser, 30)
+        # press play on the video if possible
+        try:
+            wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='ytp-play-button ytp-button']"))).click()
+        except TimeoutException:
+            pass
         # check whether video is skippable; if so, wait until skip button appears and click it
         try:
             preskipbutton = self.browser.find_element_by_xpath("//div[contains(@id, 'preskip-component')]")
@@ -227,7 +230,7 @@ class YoutubeFollower():
                 time.sleep(1)
 
         self.skip_ads()
-        time.sleep(15)  # watch some of the video
+        time.sleep(5)  # watch some of the video
         recs = self.parse_page()
 
         self.search_info[video_id] = {'recommendations': recs,
