b3d3540b2ea43b31bad7f019ef526ba2b91a9fbe
Charlie Walker <cwalker4@stanford.edu>
2019-08-09 17:42:47 -0700

    set up mini scrape script

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from youtube_follower.utils import get_top_news_videos
 
 root_videos = get_top_news_videos()
 
-yf = youtube_follower.YoutubeFollower(
-    outdir='test'
-    )
-
 for video_id in root_videos:
-	yf.run(video_id)
+    yf = youtube_follower.YoutubeFollower(
+            n_splits=3,
+            depth=20,
+            const_depth=5,
+            sample=True,
+            outdir='data/scrape_results')
+    yf.run(video_id)
+
