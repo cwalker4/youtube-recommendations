1645075e1466930bfda1a5b8ebd51d30cbcb9404
Charlie Walker <cwalker4@stanford.edu>
2019-08-13 21:55:36 -0700

    create utils.R

---
+++ scripts/analysis/utils.R
@@ -0,0 +1,8 @@
+# deal with python-formatted lists
+format_python_lists <- function(rec_str) {
+  if (rec_str == "[]") {
+    return(NA)
+  }
+  rec_str <- str_replace_all(rec_str, "[\\[\\]\\s']", '')
+  return(strsplit(rec_str, ','))
+}
