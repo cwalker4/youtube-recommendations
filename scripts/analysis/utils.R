3731972e0b283ee1bd5bad8d712eaa69a9714ac6
Charlie Walker <cwalker4@stanford.edu>
2019-08-18 12:45:33 -0400

    fix utils function

--- scripts/analysis/utils.R
+++ scripts/analysis/utils.R
@@ -1,6 +1,6 @@
 # deal with python-formatted lists
 format_python_lists <- function(rec_str) {
-  if (rec_str == "[]") {
+  if (rec_str == "[]" | is.na(rec_str)) {
     return(NA)
   }
   rec_str <- str_replace_all(rec_str, "[\\[\\]\\s']", '')
