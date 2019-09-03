17cfb686c53a6f5c983858c5cb109c669963260e
Charlie Walker <cwalker4@stanford.edu>
2019-09-02 18:16:06 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,56 @@
+import os
+import fnmatch
+
+import sqlite3
+from sqlite3 import Error
+
+
+def credentials():
+	"""
+	Set up credentials folder and empty api key text file
+	"""
+	os.mkdir('credentials')
+	open('credentials/api_key.txt', 'w').close()
+
+
+def execute_sql_script(cur, path):
+	"""
+	Executes the script at path. Handles multiple commands per file.
+
+	"""
+	f = open(path, 'r')
+	all_cmds = f.read()
+	f.close()
+
+	all_cmds = all_cmds.split(';')
+	for sql in all_cmds:
+		cur.execute(sql)
+
+
+def database():
+	"""
+	set up the data directory
+
+	"""
+	os.mkdir('data')
+	try:
+		con = sqlite3.connect('data/crawl.sqlite')
+	except Error as e:
+		print(e)
+	else:
+		cur = con.cursor()
+		for file in os.listdir('scripts/data_preparation'):
+			if fnmatch.fnmatch(file, '*.sql'):
+				path = os.path.join('scripts/data_preparation', file)
+				execute_sql_script(cur, path)
+	finally:
+		con.commit()
+		con.close()
+
+
+if __name__ == "__main__":
+	database()
+	credentials()
+	print("Done.\n")
+	print("Copy your YouTube API key into `credentials/api_key.txt`")
+
